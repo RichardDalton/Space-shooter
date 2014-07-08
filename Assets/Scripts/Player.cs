0fdd5f8e31ed1e1c572b3183559d0aa163a48297
Richard Dalton <rbdalton@gmail.com>
2014-07-08 20:46:19 +0100

    Initial files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Transform myTransform;
+
+	public int playerSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		// Spawn point
+
+		myTransform = transform;
+
+		myTransform.position = new Vector3 (-3, -3, -1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTransform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * playerSpeed);
+	}
+}

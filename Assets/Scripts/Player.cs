40fdecff6f44165fc9ed172948ae622e1d5f4f8e
Richard Dalton <rbdalton@gmail.com>
2014-07-08 22:26:20 +0100

    Finished tutorial 6

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,44 @@ public class Player : MonoBehaviour {
 
 	private Transform myTransform;
 
-	public int playerSpeed = 5;
+	public int playerSpeed = 15;
+
+	// variable to reference prefab - prefab = gameobject
+
+	public GameObject ProjectileFab;
+
 
 	// Use this for initialization
 	void Start () {
-		// Spawn point
 
 		myTransform = transform;
 
-		myTransform.position = new Vector3 (-3, -3, -1);
+		// Spawn point
+		myTransform.position = new Vector3 (0, -3, -1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		myTransform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * playerSpeed);
+		myTransform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * playerSpeed * Time.deltaTime);
+
+		Vector3 position = myTransform.position;
+
+		if (position.x > 6.5f) {
+			myTransform.position = new Vector3(-6.5f, position.y, position.z);
+		} else if (position.x < -6.5f) {
+			myTransform.position = new Vector3(6.5f, position.y, position.z);
+		}
+
+		// press space bar to fire a laser
+
+		// if the player presses down the space bar a laser will shoot out
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			Vector3 projectilePosition = new Vector3(position.x, -2, position.z);
+
+			// file projectile
+			Instantiate(ProjectileFab, projectilePosition, Quaternion.identity);
+		}
 	}
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	private Transform myTransform;
+	public int projectileSpeed = 3;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (myTransform.position.y > 7) {
+			DestroyObject(this.gameObject);
+		}
+
+		myTransform.Translate (Vector3.up * Time.deltaTime * projectileSpeed);
+	}
+}
